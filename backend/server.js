8bdd2bbc0c5a76b2a212f4c8ecb53dd28581ad50
AbeysiriLasantha <lasantha.abeysisri@gmail.com>
2024-10-02 23:13:51 -0500

    Initialized the connection to the MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,32 @@
-require('dotenv').config();
-const express = require ('express');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const connectToDb = require("./config/connectToDB"); // Database connection function
+
+
 const app = express();
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
+
+// Express middleware
+app.use(express.json());
 
 
-app.listen (PORT, ()=> {
-    console.log (`Server is running on port: ${PORT}`)
-});
+// Async function to handle database connection and server setup
+const startServer = async () => {
+  try {
+    // Separate block: Database connection
+    console.log("Connecting to the database...");
+        await connectToDb();
+    console.log("Database connected successfully.");
 
+    // Starting the server
+    app.listen(PORT, () => {
+      console.log(`Express Server: Running - Port: ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Failed to connect to the database or start server:", error);
+  }
+};
+
+// Execute the startServer function
+startServer();
